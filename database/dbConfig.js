e2a6a1a844c909a01ed285b45e9d480caa45d37f
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-28 11:55:35 -0500

    dbConfig object using dynamic environment variable for testing.

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,9 @@
 const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
+const config = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const dbEnv = process.DB_ENV || 'development'
+
+module.exports = knex(config[dbEnv])
+
+
